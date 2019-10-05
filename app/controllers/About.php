9f4c2dc5a3581aa5638a7e282ea65d5b9f0946c9
Levandri Ajmal <levandriajmal@gmail.com>
2019-10-05 21:48:51 +0700

    Membuat controller dan method default

---
+++ app/controllers/About.php
@@ -0,0 +1,12 @@
+<?php 
+
+class About {
+	public function index($nama = 'Ajmal', $pekerjaan = 'Pelajar')
+	{
+		echo "Halo, nama saya $nama, saya adalah seorang $pekerjaan";
+	}
+	public function page()
+	{
+		echo 'About/page';
+	}
+}
