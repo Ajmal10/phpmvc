3f1491ab613c44fa42e8051c4eae566ab25b5f5b
Levandri Ajmal <levandriajmal@gmail.com>
2019-10-06 08:29:07 +0700

    Menambahkan parameter umur dimethod index, class about

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,9 +1,9 @@
 <?php 
 
 class About {
-	public function index($nama = 'Ajmal', $pekerjaan = 'Pelajar')
+	public function index($nama = 'Ajmal', $pekerjaan = 'Pelajar', $umur = 20)
 	{
-		echo "Halo, nama saya $nama, saya adalah seorang $pekerjaan";
+		echo "Halo, nama saya $nama, saya adalah seorang $pekerjaan. Saya berumur $umur tahun";
 	}
 	public function page()
 	{
