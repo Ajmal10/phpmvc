aa1f2cacdd22f7fa30ff648950dc9480f4a90e41
Levandri Ajmal <levandriajmal@gmail.com>
2019-10-06 16:11:06 +0700

    data mahasiswa nya menggunakan database

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -2,29 +2,52 @@
 
 
 class Mahasiswa_model {
-	private $mhs = [
-		[ 
-			"nama" => "Levandri Ajmal",
-			"nrp" => "173040147",
-			"email" => "Levandriajmal@gmail.com",
-			"jurusan" => "Teknik Informatika"
-		],
-		[
-			"nama" => "Ahmad Fauzan",
-			"nrp" => "173040148",
-			"email" => "AhmadFauzan@gmail.com",
-			"jurusan" => "Teknik Mesin"
-		],
-		[
-			"nama" => "Fauzan Ajmal",
-			"nrp" => "173040149",
-			"email" => "Fauzanajmal@gmail.com",
-			"jurusan" => "Teknik Lingkungan"
-		]
-	];
+
+	private $dbh; // database handler
+	private $stmt;
+
+	public function __construct()
+	{
+		// data source name
+		$dsn = 'mysql:host=localhost;dbname=phpmvc';
+
+		try {
+			$this->dbh = new PDO($dsn, 'root', '');
+		} catch(PDOException $e) {
+			die($e->getMassage());
+		}
+	}
 
 	public function getAllMahasiswa()
 	{
-		return $this->mhs;
+		$this->stmt = $this->dbh->prepare('SELECT * FROM mahasiswa');
+		$this->stmt->execute();
+		return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
 	}
+
+	// private $mhs = [
+	// 	[ 
+	// 		"nama" => "Levandri Ajmal",
+	// 		"nrp" => "173040147",
+	// 		"email" => "Levandriajmal@gmail.com",
+	// 		"jurusan" => "Teknik Informatika"
+	// 	],
+	// 	[
+	// 		"nama" => "Ahmad Fauzan",
+	// 		"nrp" => "173040148",
+	// 		"email" => "AhmadFauzan@gmail.com",
+	// 		"jurusan" => "Teknik Mesin"
+	// 	],
+	// 	[
+	// 		"nama" => "Fauzan Ajmal",
+	// 		"nrp" => "173040149",
+	// 		"email" => "Fauzanajmal@gmail.com",
+	// 		"jurusan" => "Teknik Lingkungan"
+	// 	]
+	// ];
+
+	// public function getAllMahasiswa()
+	// {
+	// 	return $this->mhs;
+	// }
 }
