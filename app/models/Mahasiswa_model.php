ec0fd11eaa858c85a238178fb4f8b0041540120b
Levandri Ajmal <levandriajmal@gmail.com>
2019-10-06 22:01:36 +0700

    Menambahkan fitur delete

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -39,6 +39,18 @@ class Mahasiswa_model {
 		return $this->db->rowCount();
 	}
 
+	public function hapusDataMahasiswa($id)
+	{
+		$query = "DELETE FROM mahasiswa WHERE id = :id";
+		$this->db->query($query);
+		$this->db->bind('id', $id);
+
+		$this->db->execute();
+
+		return $this->db->rowCount();
+	}
+
+
 	// private $mhs = [
 	// 	[ 
 	// 		"nama" => "Levandri Ajmal",
