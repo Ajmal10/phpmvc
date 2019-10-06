3930623afabc9684a54bd0f87655b7d169681eb5
Levandri Ajmal <levandriajmal@gmail.com>
2019-10-06 10:23:52 +0700

    Mengelola Assets dan Menambahkan templates bootstrap

---
+++ app/core/Constants.php
@@ -0,0 +1,4 @@
+<?php 
+
+
+define('BASEURL', 'http://localhost/REKWEB/phpmvc/public');
--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,8 @@
+<div class="container">
 
-	<h1>About Me</h1>
+	<h1 class="mt-4">About Me</h1>
+	<img src=" <?= BASEURL; ?>/img/ajmal.jpg " alt = "Levandri ajmal" width="200" class="rounded-circle shadow" >
 	<p>Halo, nama saya <?= $data['nama'];?>, umur saya <?= $data['umur'];?> tahun, saya adalah seorang <?= $data['pekerjaan'];?></p>
+
+
+</div>
